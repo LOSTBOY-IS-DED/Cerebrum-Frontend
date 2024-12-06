d11969d1ab5c7b2177bc1658e96cc923704f802c
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-12-06 06:36:52 +0530

    added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,49 @@ import { Card } from "./components/Card";
 import { CreateContentModal } from "./components/CreateContentModal";
 import { PlusIcon } from "./icons/PlusIcon";
 import { ShareIcon } from "./icons/ShareIcon";
+import { SideBar } from "./components/Sidbar";
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
 
   return (
-    <div className="p-4">
-      <CreateContentModal
-        open={modalOpen}
-        onClose={() => {
-          setModalOpen(false);
-        }}
-      />
-      <div className="flex justify-end gap-4">
-        <Button
-          onClick={() => {
-            setModalOpen(true);
+    <div>
+      <SideBar />
+      <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
+        <CreateContentModal
+          open={modalOpen}
+          onClose={() => {
+            setModalOpen(false);
           }}
-          variant="secondary"
-          text="Add-content"
-          startIcon={<PlusIcon />}
         />
-        <Button
-          variant="primary"
-          text="share-Brain"
-          startIcon={<ShareIcon />}
-        />
-      </div>
+        <div className="flex justify-end gap-4">
+          <Button
+            onClick={() => {
+              setModalOpen(true);
+            }}
+            variant="secondary"
+            text="Add-content"
+            startIcon={<PlusIcon />}
+          />
+          <Button
+            variant="primary"
+            text="share-Brain"
+            startIcon={<ShareIcon />}
+          />
+        </div>
 
-      <div className="flex gap-4">
-        <Card
-          title="My twitter post"
-          type="twitter"
-          link="https://x.com/CalvinnChenn/status/1864449859891216419"
-        ></Card>
-        <Card
-          title="My youtube video"
-          type="youtube"
-          link="https://www.youtube.com/watch?v=byitAI7kkOM"
-        ></Card>
+        <div className="flex gap-4">
+          <Card
+            title="My twitter post"
+            type="twitter"
+            link="https://x.com/CalvinnChenn/status/1864449859891216419"
+          ></Card>
+          <Card
+            title="My youtube video"
+            type="youtube"
+            link="https://www.youtube.com/watch?v=byitAI7kkOM"
+          ></Card>
+        </div>
       </div>
     </div>
   );
